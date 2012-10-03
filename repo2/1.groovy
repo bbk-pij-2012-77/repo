142bbd519b9a9d4b5ae00a53ae0ba9bcf98e3ac2
bbk-pij-2012-77 <htshin@gmail.com>
2012-10-03 15:39:00 +0100

    Second

---
+++ repo2/1.groovy
@@ -0,0 +1,18 @@
+#asfafasfd
+Testtestestestsetsetsetestsetse
+tring str;
+println "Write the blooming number: ";
+str = System.console().readLine();
+int i = Integer.parseInt(str);
+int j;
+j = 0;
+while (j < i){
+	if (i % j == 0){
+		println "Not prime";
+		break;
+	} else {
+	j = j + 1;
+	}
+	
+}
+println "it must be a prime ! ";
